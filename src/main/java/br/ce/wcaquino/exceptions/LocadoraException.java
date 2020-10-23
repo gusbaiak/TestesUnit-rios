ff64bf3e600f5463d62577b1ebdc7e677315bbd3
Gustavo Baiak <gustavobak15@hotmail.com>
2020-10-23 16:06:24 -0300

    Projeto Finalizado

---
+++ src/main/java/br/ce/wcaquino/exceptions/LocadoraException.java
@@ -0,0 +1,11 @@
+package br.ce.wcaquino.exceptions;
+
+public class LocadoraException extends Exception {
+
+	private static final long serialVersionUID = -7389716283027635483L;
+
+	public LocadoraException(String message) {
+		super(message);
+	}
+
+}
